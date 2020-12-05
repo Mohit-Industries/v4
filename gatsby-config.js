459eed5542b88b96175f8764718b34ab1f8898f2
Mohit Varikuti <65371337+GitHubEmploy@users.noreply.github.com>
2020-12-05 13:18:27 -0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Mohit Varikuti',
     description:
-      'Brittany Chiang is a software engineer specializing in building (and occasionally designing) exceptional websites, applications, and everything in between.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Mohit Varikuti is a 13 year old AI expert, and occasionally designer. I can make exceptional websites, applications, and everything in between.',
+    siteUrl: 'https://mohitvarikuti.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@MohitVarikuti',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -19,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'MohitVarikuti',
+        short_name: 'MohitReddy',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
