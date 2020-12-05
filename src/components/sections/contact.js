29c774be63762272e260bdfe33dcece3cd3b5602
Mohit Varikuti <65371337+GitHubEmploy@users.noreply.github.com>
2020-12-05 13:24:22 -0800

    Update contact.js

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -51,8 +51,7 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I'm not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I'll try my best to get back to you!
+        I am always looking for new opportunities and things to do, so if you ever need a Custom job, or just want to talk, my Inbox is always open :)
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
