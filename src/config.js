8c8b27bdd23fb82ad935da9272772826753faee7
Mohit Varikuti <65371337+GitHubEmploy@users.noreply.github.com>
2020-12-05 13:45:32 -0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,26 +1,22 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'mohit.varikuti@gmail.com',
 
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/GitHubEmploy',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'NotYetAvalibale',
     },
     {
       name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
-    },
-    {
-      name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
+      url: 'https://codepen.io/githubemploy',
     },
     {
       name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://twitter.com/MohitVarikuti',
     },
   ],
 
