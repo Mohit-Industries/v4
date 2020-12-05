d499188aab295fa1ed068cb981066eef61f8b1a1
Mohit Varikuti <65371337+GitHubEmploy@users.noreply.github.com>
2020-12-05 13:20:10 -0800

    Update menu.js

--- src/components/menu.js
+++ src/components/menu.js
@@ -260,7 +260,7 @@ const Menu = () => {
               </ol>
             )}
 
-            <a href="/resume.pdf" className="resume-link">
+            <a href="https://mohit.varikuti.repl.co" className="resume-link">
               Resume
             </a>
           </nav>
