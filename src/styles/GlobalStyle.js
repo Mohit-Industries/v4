e5403febe0460650eae5d4307b7acaa6260f3a97
Mohit Varikuti <65371337+GitHubEmploy@users.noreply.github.com>
2021-01-30 23:54:29 -0800

    Update GlobalStyle.js

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -14,7 +14,7 @@ const GlobalStyle = createGlobalStyle`
     --navy-shadow: rgba(2, 12, 27, 0.7);
     --slate: #8892b0;
     --light-slate: #a8b2d1;
-    --lightest-slate: #ccd6f6;
+    --lightest-slate: #8892b0;
     --white: #e6f1ff;
     --green: #64ffda;
     --green-tint: rgba(100, 255, 218, 0.1);
