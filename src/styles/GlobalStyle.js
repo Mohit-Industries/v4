9bfed9e560d2ff3ce1169c5d2d47974f931fcef9
Mohit Varikuti <65371337+GitHubEmploy@users.noreply.github.com>
2021-01-31 01:43:52 -0800

    Update GlobalStyle.js

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -7,16 +7,16 @@ const GlobalStyle = createGlobalStyle`
   ${Fonts};
 
   :root {
-    --dark-navy: #FFFFF;
-    --navy: #FFFFF;
-    --light-navy: #FFFFF;
-    --lightest-navy: #FFFFF;
-    --navy-shadow: rgba(138, 146, 173, 0.7);
-    --slate: #a7b0cc;
-    --light-slate: #8790ad;
-    --lightest-slate: #8892b0;
+    --dark-navy: #020c1b;
+    --navy: #0a192f;
+    --light-navy: #112240;
+    --lightest-navy: #233554;
+    --navy-shadow: rgba(2, 12, 27, 0.7);
+    --slate: #8892b0;
+    --light-slate: #a8b2d1;
+    --lightest-slate: #ccd6f6;
     --white: #e6f1ff;
-    --green: #00ff9d;
+    --green: #64ffda;
     --green-tint: rgba(100, 255, 218, 0.1);
 
     --font-sans: 'Calibre', 'San Francisco', 'SF Pro Text', -apple-system, system-ui, sans-serif;
